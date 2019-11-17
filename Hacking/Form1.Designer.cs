4a95db00a0cd5196b7787bf6ef6577d6b948186d
ilyaDubovtsev <dubovtsev98@gmail.com>
2019-11-17 16:30:29 +0400

    implimentation

---
+++ Hacking/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Hacking
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputedText = new System.Windows.Forms.TextBox();
+            this.CryptedText = new System.Windows.Forms.TextBox();
+            this.txtBPublicKey = new System.Windows.Forms.Label();
+            this.txtBSecretKey = new System.Windows.Forms.Label();
+            this.DecryptedText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // InputedText
+            // 
+            this.InputedText.Location = new System.Drawing.Point(27, 69);
+            this.InputedText.Multiline = true;
+            this.InputedText.Name = "InputedText";
+            this.InputedText.Size = new System.Drawing.Size(200, 223);
+            this.InputedText.TabIndex = 1;
+            this.InputedText.TextChanged += new System.EventHandler(this.InputedText_TextChanged);
+            // 
+            // CryptedText
+            // 
+            this.CryptedText.Location = new System.Drawing.Point(294, 69);
+            this.CryptedText.Multiline = true;
+            this.CryptedText.Name = "CryptedText";
+            this.CryptedText.Size = new System.Drawing.Size(200, 223);
+            this.CryptedText.TabIndex = 2;
+            // 
+            // txtBPublicKey
+            // 
+            this.txtBPublicKey.AutoSize = true;
+            this.txtBPublicKey.Location = new System.Drawing.Point(121, 316);
+            this.txtBPublicKey.Name = "txtBPublicKey";
+            this.txtBPublicKey.Size = new System.Drawing.Size(87, 13);
+            this.txtBPublicKey.TabIndex = 3;
+            this.txtBPublicKey.Text = "Открытый ключ";
+            // 
+            // txtBSecretKey
+            // 
+            this.txtBSecretKey.AutoSize = true;
+            this.txtBSecretKey.Location = new System.Drawing.Point(121, 351);
+            this.txtBSecretKey.Name = "txtBSecretKey";
+            this.txtBSecretKey.Size = new System.Drawing.Size(87, 13);
+            this.txtBSecretKey.TabIndex = 4;
+            this.txtBSecretKey.Text = "Закрытый ключ";
+            // 
+            // DecryptedText
+            // 
+            this.DecryptedText.Location = new System.Drawing.Point(559, 69);
+            this.DecryptedText.Multiline = true;
+            this.DecryptedText.Name = "DecryptedText";
+            this.DecryptedText.Size = new System.Drawing.Size(200, 223);
+            this.DecryptedText.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(791, 450);
+            this.Controls.Add(this.DecryptedText);
+            this.Controls.Add(this.txtBSecretKey);
+            this.Controls.Add(this.txtBPublicKey);
+            this.Controls.Add(this.CryptedText);
+            this.Controls.Add(this.InputedText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox InputedText;
+        private System.Windows.Forms.TextBox CryptedText;
+        private System.Windows.Forms.Label txtBPublicKey;
+        private System.Windows.Forms.Label txtBSecretKey;
+        private System.Windows.Forms.TextBox DecryptedText;
+    }
+}
+
---
+++ Hacking/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hacking
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void InputedText_TextChanged(object sender, EventArgs e)
+        {
+            var strIn = InputedText.Text;
+            var p = Numbers.GetNextPrimeAfter(1);
+            var g = Numbers.GetPRoot(p);
+            var x = Numbers.Rand(1, p - 1);
+            Crypt(p, g, x, strIn);
+            var strOut = CryptedText.Text;
+            Decrypt(p, x, strOut);
+        }
+
+        private void Crypt(int p, int g, int x, string strIn) //Шифрование
+        {
+            var y = Numbers.Power(g, x, p);
+            
+            CryptedText.Text = "";
+            txtBPublicKey.Text = $"Открытый ключ (p,g,y) = ( {p}, {g}, {y})";
+            txtBSecretKey.Text = $"Закрытый ключ x = {x}";
+
+            var sb = new StringBuilder();
+            
+            foreach (var t in strIn)
+            {
+                var m = Convert.ToInt32(t);
+                if (m <= 0) continue;
+                
+                var k = Numbers.Rand(1, p - 1);
+                var a = Numbers.Power(g, k, p);
+                var b = Numbers.Mul(Numbers.Power(y, k, p), m, p);
+                
+                sb.Append($"{a} {b} ");
+            }
+            
+            CryptedText.Text = sb.ToString();
+        }
+
+        private void Decrypt(int p, int x, string strIn)
+        {
+            DecryptedText.Text = "";
+            if (strIn.Length > 0)
+            {
+                var sb = new StringBuilder();
+                var crypted = strIn.Trim().Split(' ').Select(int.Parse).ToArray();
+
+                for (var i = 0; i < crypted.Length - 1; i += 2)
+                {
+                    var a = crypted[i];
+                    var b = crypted[i + 1];
+
+                    if ((a == 0) || (b == 0)) continue;
+
+                    var deM = Numbers.Mul(b, Numbers.Power(a, p - 1 - x, p),p); // m=b*(a^x)^(-1)mod p =b*a^(p-1-x)mod p - трудно было  найти нормальную формулу, в ней вся загвоздка
+                    Console.WriteLine(deM);
+                    sb.Append(Convert.ToChar(deM));
+                }
+
+                DecryptedText.Text = sb.ToString();
+                return;
+            }
+
+            DecryptedText.Text = "";
+        }
+    }
+}
